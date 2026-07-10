a70b534932a2e0dad72c92d78143d4647b434e02
Benjamin Chong <ben.chong0248@gmail.com>
2026-07-09 22:20:41 -0700

    Added the diff generation for preprocessing

---
+++ features/diff.py
@@ -0,0 +1,11 @@
+from difflib import ndiff
+
+def generate_diff(buggy_code, fixed_code): #goal is to create the difference between buggy vs fixed
+    #design decisions:
+    #1. remove the visual ? and lines where no changes to reduce noise
+    buggy_code = buggy_code.splitlines() #splits based off of lines for line diff 
+    fixed_code = fixed_code.splitlines()
+    diff = list(ndiff(buggy_code, fixed_code))
+    diff = [line for line in diff if line.startswith('+') or line.startswith('-')]
+    diff = '\n'.join(diff)
+    return diff
