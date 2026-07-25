44251561b432e1820802033845f8135f91d85e29
Benjamin Chong <ben.chong0248@gmail.com>
2026-07-24 22:44:59 -0700

    Fixed working directory and updated it accordingly / Finalized tokenizer and Vocab Exploration

---
+++ src/v2/comment_only_diff.py
@@ -0,0 +1,14 @@
+def is_comment_only(diff):
+    lines = diff.splitlines()
+
+    for line in lines:
+        if not line.startswith(('+','-')):
+            continue
+        else:
+            line = line.replace(line[0], '', 1)
+        line = line.strip()
+        if not line:
+            continue
+        if not line.startswith('#'):
+            return False
+    return True
--- src/v2/tokenize_diff.py
+++ src/v2/tokenize_diff.py
@@ -1,29 +1,45 @@
 import tokenize
 import io
+#Note: Indentation was normalized into four space levels. Each partial level is rounded down
+
+def indentation_level(line):
+    tabs = line.count('\t')
+    level = (len(line) - len(line.lstrip(' '))) // 4
+    return f'<INDENT_{tabs + level}>'
+
 
 def tokenize_diff(changes):
     tokens = []
     lines = changes.splitlines()
 
     for line in lines:
-        line = line.strip()
+        line_tokens = []
         if not line:
             continue
+        elif not line.startswith(('+ ', '- ')):
+            continue
+
         holder = ''
         if line[0] == '-':
             holder = '<DELETE>'
-            line = line[1:].strip()
+            line = line[2:]
         elif line[0] == '+':
             holder = '<ADD>'
-            line = line[1:].strip()
-        if holder:
-            tokens.append(holder)
+            line = line[2:]
+        print(line)
+        indentation = indentation_level(line)
+        line = line.strip() #strip after getting the indent level
+        
         try:
             token_generator = tokenize.generate_tokens(io.StringIO(line).readline)
             for token in token_generator:
-                if token.type == tokenize.ENDMARKER or token.type == tokenize.NEWLINE or token.type == tokenize.NL:
+                if token.type in (tokenize.ENDMARKER, tokenize.NEWLINE, tokenize.NL, tokenize.COMMENT):
                     continue
-                tokens.append(token.string)
+                line_tokens.append(token.string)
+            if holder and line_tokens:
+                tokens.append(holder)
+                tokens.append(indentation)
+            tokens += line_tokens
         except tokenize.TokenError:
             pass
 
