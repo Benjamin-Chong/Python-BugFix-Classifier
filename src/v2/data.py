9037b7efbe72020715762d698f8840987cf4da43
Benjamin Chong <ben.chong0248@gmail.com>
2026-08-08 19:45:17 -0700

    Cleaned up some of 04_model

---
+++ src/v2/data.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+class BugFixDataset(Dataset): #adapterr that makes our pandas data understandabnle to PyTorch
+    def __init__(self, dataframe):
+        super().__init__()
+        self.dataframe = dataframe
+    def __len__(self):
+        return len(self.dataframe)
+    def __getitem__(self, index):
+        tokens_in_ids = self.dataframe['tokens_ids'].iloc[index]
+        label_id = self.dataframe['label_id'].iloc[index]
+        return torch.tensor(tokens_in_ids, dtype=torch.long), torch.tensor(label_id, dtype=torch.long)
+
+
+def my_collate_fn(batch): #batch has tensors for tokens, labels. used to create a rectangular shape so matrix operations can be applied
+    token_tensors, label_tensors = zip(*batch) #after zipping both return the tensors and the labels
+    batch_max_length = 0 #find the max length for proper matrix multiplication (shape)
+    for tokens in token_tensors: #find the max length
+        batch_max_length = max(batch_max_length, len(tokens))
+
+    padded_token_tensors = []
+    for tokens in token_tensors: #adds padding where needed and puts it into the padded token tensor list
+        amount_of_padding = batch_max_length - len(tokens)
+        pads = torch.zeros(amount_of_padding, dtype=torch.long, device=tokens.device) #adds 0s
+        padded_sequence = torch.cat((tokens, pads)) #torch.cat returns a new object
+        assert torch.equal(padded_sequence[:len(tokens)], tokens) #ensures that the padded sequence real tokens and the original tokens are still equel
+        padded_token_tensors.append(padded_sequence)
+
+    stacked_padded_tokens = torch.stack(padded_token_tensors) #list -> 1 tensor
+    labels = torch.stack(label_tensors) #tensors individually -> 1 tensor
+
+    padding_mask = stacked_padded_tokens == 0 #creates a mask holding True for pads and False for real tokens
+
+    return stacked_padded_tokens, labels, padding_mask
---
+++ src/v2/model.py
@@ -0,0 +1,77 @@
+import torch.nn as nn
+import torch
+
+class BugFixClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embeddings = nn.Embedding(num_embeddings=947, embedding_dim=128, padding_idx=0)
+        self.classifier = nn.Linear(128,5) #Layer creation
+
+    def forward(self, tokens, padding_mask):
+        embedded_tokens = self.embeddings(tokens) # starts as: (batchsize, maxlen) -> (batchsize, maxlen, embeddingdim)
+        real_token_mask = (~padding_mask).int() #starts as: (batchsize, maxlen) -> (batchsize, maxlen) results in 1s/0s each diff has a vector of 1s/0s rather than the True and False
+        unsqueezed = real_token_mask.unsqueeze(dim=2) #starts as: (batchsize, maxlen) -> (batchsize, maxlen, 1) for broadcasting later
+        masked = embedded_tokens * unsqueezed #results: (batchsize, maxlen, embeddingdim) retrains values for embeddings that are real tokens. pads are reduced to 0
+
+        token_sums = masked.sum(dim=1) #sum all of the embeddings
+        token_counts = unsqueezed.sum(dim=1) #sum all of the real tokens (True = 1, False = 0)
+
+        mean_pooling = token_sums / token_counts #results: (batchsize, maxlen) results in a tensor using broadcasting
+        logits = self.classifier(mean_pooling) #results: (batchsize, scores) pass data through the layer
+        return logits
+
+def train_model(model, train_loader, validation_loader, criterion, optimizer, epochs, checkpoint_path='models/v2_best_model.pt', print_stats=True):
+    #Pipeline Overview:
+    #1 Predict 
+    #2 Compute Loss (criterion)
+    #3 Apply Backpropagation
+    #4 Update Weights (Optimzer)
+
+    best_validation_loss = float('inf') #used for saving the best model later
+    for epoch in range(epochs):
+        model.train() #put the model into train mode
+        total_loss = 0
+        total_correct = 0
+        total_examples = 0
+
+        for tokens, labels, padding_mask in train_loader:
+            optimizer.zero_grad() #resets the gradient
+
+            logits = model(tokens, padding_mask) #forward pass
+            loss = criterion(logits, labels) #compute loss
+            
+            loss.backward() #apply backpropagation
+            optimizer.step() #change weights
+
+            predictions = logits.argmax(dim=1) #take the largest score
+            total_correct += (predictions == labels).sum().item()
+            total_examples += labels.size(0)
+            total_loss += loss.item() * labels.size(0)
+
+        train_loss = total_loss / total_examples
+        train_accuracy = total_correct / total_examples
+
+        model.eval() #put the model into evaluation mode
+
+        validation_loss = 0
+        validation_correct = 0
+        validation_examples = 0
+
+        with torch.no_grad(): #no gradient needed since no weights are changing
+            for tokens, labels, padding_mask in validation_loader:
+                logits = model(tokens, padding_mask) #forward pass
+                loss = criterion(logits, labels)  #compute loss
+
+                predictions = logits.argmax(dim=1)
+                validation_loss += loss.item() * labels.size(0)
+                validation_correct += (predictions == labels).sum().item()
+                validation_examples += labels.size(0)
+
+        validation_epoch_loss = validation_loss / validation_examples
+        validation_accuracy = validation_correct / validation_examples
+        if validation_epoch_loss < best_validation_loss:
+            best_validation_loss = validation_epoch_loss
+            torch.save({'epoch': epoch + 1, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'validation_loss': validation_epoch_loss}, checkpoint_path)
+
+        if print_stats:
+            print(f'Epoch: {epoch + 1}, Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f} Validation Loss: {validation_epoch_loss:.4f} Validation Accuracy: {validation_accuracy:.4f}')
